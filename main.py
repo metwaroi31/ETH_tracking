07ef19abe5cfa2f270430dde7487b0366f4edb8c
Bùi Tuấn Anh <metwaroi31@gmail.com>
2021-09-04 18:18:16 +0700

    setup docker and refactor code

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from api_calling.services.currency_services import get_currency_list
 from api_calling.services.platform_token_services import get_platform_token_list, get_exchange_rate_token_realtime
 from api_calling.reponse_json_format import CURRENCY, PLATFORM_TOKEN, EXCHANGE_RATE_REALTIME
 
-# get_currency_list(CURRENCY)
-# get_platform_token_list(PLATFORM_TOKEN)
-print (get_exchange_rate_token_realtime("ethereum", "0xe41d2489571d322189246dafa5ebde1f4699f498", EXCHANGE_RATE_REALTIME))
+# TO DO : tranform into excel files based on DDL
+
+parameters_price_api = {
+    "contract_addresses" : "0xe41d2489571d322189246dafa5ebde1f4699f498",
+    "vs_currencies" : "usd",
+    "include_market_cap" : "true",
+    "include_24hr_vol" : "true",
+    "include_24hr_change" : "true",
+    "include_last_updated_at" : "true",
+}
+
+parameters_platform_token_list = {
+    "include_platform" : "true"
+}
+# print (get_currency_list(model=CURRENCY))
+# print (get_platform_token_list(parameters=parameters_platform_token_list,model=PLATFORM_TOKEN))
+print (get_exchange_rate_token_realtime(parameters=parameters_price_api, model=EXCHANGE_RATE_REALTIME))
